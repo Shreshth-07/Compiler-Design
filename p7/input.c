6eecc536326c3c32e9fdfe9e9d31ee49edec8b39
Shreshth Pratap Singh <114429628+Shreshth-07@users.noreply.github.com>
2023-06-16 19:23:11 +0530

    Add files via upload

---
+++ p7/input.c
@@ -0,0 +1,13 @@
+//these are header file and used to import library from the header files
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	/*this is man fuction and compiler only read main fuction at first
+	  during compiling process*/
+	printf("HELLO WORLD.....");
+	return 0;
+	//this is us bhai us
+
+}
